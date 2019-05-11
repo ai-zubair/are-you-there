84705c3eb3102a3591338d5ce8b4dc0ca163d6c1
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-05-11 20:52:13 +0530

    remove console log statement

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,6 @@ socket.on('newMsg',(msg)=>{
     console.log('New msg has been recieved!');
     const newMsg = `<h3>FROM: ${msg.from}</h3><p>${msg.text}</p>`;
     const messages=document.getElementById('messages');
-    console.log(messages)
     messages.insertAdjacentHTML('beforeend',newMsg);
     console.log(msg);
 })
