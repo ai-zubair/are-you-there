5ffc4afde94f7b24d4af8c3e288c7b02ff418fe1
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-05-11 14:45:50 +0530

    setting up socketIO on sever and client

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
+const path = require('path'); //to get rid of the path style-problem when serving static content
+const http = require('http'); //to have explicit access to the http.Server instance 
 const express = require('express');
-const path = require('path')
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname,'..','public');
+const publicPath = path.join(__dirname,'..','public'); //better styled path to use for static assets
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+const server = http.createServer(app); //creating an instance of http.Server
+const io = socketIO(server); //setting up this Server as web socket server
 
 app.use(express.static(publicPath));
 
-app.listen(PORT,()=>{
+io.on('connection',(socket)=>{
+    console.log('A new user has connected!');
+    socket.on('disconnect',()=>{
+        console.log('A user has disconnected!')
+    })
+})
+
+server.listen(PORT,()=>{
     console.log(`Sever has been fired up @ port ${PORT}`)
 })
